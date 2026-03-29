38895a4b8a0edb06fdc0fc4c27faffb094c6439a
MELIODASok <153757168+MELIODASok@users.noreply.github.com>
2026-03-29 21:00:17 +0300

    Update жесты.py

--- problem-solver/samples/Жестовый_ввод/жесты.py
+++ problem-solver/samples/Жестовый_ввод/жесты.py
@@ -17,7 +17,7 @@ print(sys.executable)
 GESTURE_LANGUAGE = "ru"
 recognized_letter = ""
 # ============================================================
-
+#Нет корректного распознавания жестов
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
