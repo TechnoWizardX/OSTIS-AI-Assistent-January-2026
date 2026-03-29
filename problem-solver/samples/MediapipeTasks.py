c70f5403c0039cc58b8411b963b16ee651d9e5bb
TechnoWizardX <artyom.lagutskiy@gmail.com>
2026-03-29 20:42:45 +0300

    обновил tasks mediapipe

--- problem-solver/samples/MediapipeTasks.py
+++ problem-solver/samples/MediapipeTasks.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import cv2
 import mediapipe as mp
 
-
 MODEL_PATH = Path(__file__).with_name("hand_landmarker.task")
 
 
@@ -11,10 +10,29 @@ def draw_landmarks(frame, hand_landmarks_list):
     height, width, _ = frame.shape
 
     for hand_landmarks in hand_landmarks_list:
-        for landmark in hand_landmarks:
+        for i, landmark in enumerate(hand_landmarks):
             x = int(landmark.x * width)
             y = int(landmark.y * height)
             cv2.circle(frame, (x, y), 4, (0, 255, 0), -1)
+        pos_0 = hand_landmarks[0]
+        finger_1 = hand_landmarks[0:5]   # Большой: 0,1,2,3,4
+        finger_2 = [hand_landmarks[0]] + hand_landmarks[5:9]   # Указательный: 0, 5,6,7,8
+        finger_3 = [hand_landmarks[0]] + hand_landmarks[9:13]  # Средний: 0, 9,10,11,12
+        finger_4 = [hand_landmarks[0]] + hand_landmarks[13:17] # Безымянный: 0, 13,14,15,16
+        finger_5 = [hand_landmarks[0]] + hand_landmarks[17:21]
+        hand = [finger_1, finger_2, finger_3, finger_4, finger_5]
+        color = [(233, 150, 122), (127, 255, 0), (32, 178, 170), (240, 230, 140), (186, 85, 211)]
+        for j, finger in enumerate(hand):
+            for i in range(len(finger) - 1):
+                x1 = int(finger[i].x * width)
+                y1 = int(finger[i].y * height)
+                x2 = int(finger[i+1].x * width)
+                y2 = int(finger[i+1].y * height)
+                cv2.line(frame, (x1, y1), (x2, y2), color[j], 2)
+                
+
+
+        
 
 
 def main():
