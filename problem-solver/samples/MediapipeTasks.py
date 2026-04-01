999a987f278ddf9194438d2cc5bad3b73a3d73ea
TechnoWizardX <artyom.lagutskiy@gmail.com>
2026-04-01 19:06:17 +0300

    update жесты через tasks, сделал красивее

--- problem-solver/samples/MediapipeTasks.py
+++ problem-solver/samples/MediapipeTasks.py
@@ -10,27 +10,39 @@ def draw_landmarks(frame, hand_landmarks_list):
     height, width, _ = frame.shape
 
     for hand_landmarks in hand_landmarks_list:
-        for i, landmark in enumerate(hand_landmarks):
+        for landmark in hand_landmarks:
             x = int(landmark.x * width)
             y = int(landmark.y * height)
             cv2.circle(frame, (x, y), 4, (0, 255, 0), -1)
-        pos_0 = hand_landmarks[0]
         finger_1 = hand_landmarks[0:5]   # Большой: 0,1,2,3,4
-        finger_2 = [hand_landmarks[0]] + hand_landmarks[5:9]   # Указательный: 0, 5,6,7,8
-        finger_3 = [hand_landmarks[0]] + hand_landmarks[9:13]  # Средний: 0, 9,10,11,12
-        finger_4 = [hand_landmarks[0]] + hand_landmarks[13:17] # Безымянный: 0, 13,14,15,16
-        finger_5 = [hand_landmarks[0]] + hand_landmarks[17:21]
-        hand = [finger_1, finger_2, finger_3, finger_4, finger_5]
-        color = [(233, 150, 122), (127, 255, 0), (32, 178, 170), (240, 230, 140), (186, 85, 211)]
-        for j, finger in enumerate(hand):
+        finger_2 = hand_landmarks[5:9]   # Указательный: 0, 5,6,7,8
+        finger_3 = hand_landmarks[9:13]  # Средний: 0, 9,10,11,12
+        finger_4 = hand_landmarks[13:17] # Безымянный: 0, 13,14,15,16
+        finger_5 = hand_landmarks[17:21]
+        full_hand = [finger_1, finger_2, finger_3, finger_4, finger_5]
+        color = [(181, 228, 255), (133, 21, 199), (0, 215, 255), (50, 205, 154), (180, 130, 70)]
+        for j, finger in enumerate(full_hand):
+            for landmark in finger:
+                x = int(landmark.x * width)
+                y = int(landmark.y * height)
+                cv2.circle(frame, (x, y), 4, color[j], -1)
+        for j, finger in enumerate(full_hand):
             for i in range(len(finger) - 1):
                 x1 = int(finger[i].x * width)
                 y1 = int(finger[i].y * height)
                 x2 = int(finger[i+1].x * width)
                 y2 = int(finger[i+1].y * height)
-                cv2.line(frame, (x1, y1), (x2, y2), color[j], 2)
+                cv2.line(frame, (x1, y1), (x2, y2), color[j], 2, cv2.LINE_AA)
+        hand = [hand_landmarks[0], hand_landmarks[5], hand_landmarks[9], hand_landmarks[13], hand_landmarks[17], hand_landmarks[0]]
+        for i in range(len(hand) - 1):
+            x1 = int(hand[i].x * width)
+            y1 = int(hand[i].y * height)
+            x2 = int(hand[i+1].x * width)
+            y2 = int(hand[i+1].y * height)
+            cv2.line(frame, (x1, y1), (x2, y2), (105, 105, 105), 2, cv2.LINE_AA)
                 
-
+def check_state(hand_landmarks):
+    pass
 
         
 
@@ -48,7 +60,7 @@ def main():
     options = HandLandmarkerOptions(
         base_options=BaseOptions(model_asset_path=str(MODEL_PATH)),
         running_mode=RunningMode.VIDEO,
-        num_hands=2,
+        num_hands=4,
     )
 
     hand_landmarker = HandLandmarker.create_from_options(options)
