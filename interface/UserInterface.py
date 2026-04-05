d2cbf27e7aa9d283dbe14b1d065a174ba9997697
TechnoWizardX <artyom.lagutskiy@gmail.com>
2026-04-05 07:58:46 +0300

    добавил кнопки

--- interface/UserInterface.py
+++ interface/UserInterface.py
@@ -16,9 +16,11 @@ import sys
 class UserInterface(QMainWindow):
     def __init__(self):
         super().__init__()
+
+
         self.setWindowTitle("IAMOS")
         self.setGeometry(100, 100, 820, 690)
-        self.setMinimumSize(800, 600)
+        self.setMinimumSize(700, 525)
         self.setStyleSheet("""
                            background-color: #D9D9D9;
                            """)
@@ -28,97 +30,117 @@ class UserInterface(QMainWindow):
 
         # Основной лайаут: размещает боковую панель и панель контента горизонтально
         self.main_layout = QHBoxLayout(self.main_widget)
-        self.main_layout.setContentsMargins(10, 10, 10, 10)
-        self.main_layout.setSpacing(15)
+        self.main_layout.setContentsMargins(15, 15, 15, 15)
+        self.main_layout.setSpacing(10)
 
+        self.content_pages = [Settings(self), Profile(self),
+                               VoiceInput(self), TextInput(self),
+                               GesturesInput(self)]
+
+        self.side_panel_buttons = []
+        
+         # Панель контента(является лэйаутом)
+        self.content_panel = QStackedWidget(self.main_widget)
+        self.content_panel.setMinimumWidth(330)
+        self.content_panel.setStyleSheet("""
+            background-color: #FFFFFF;
+            border-radius: 16px;
+            """)
+    
 
         
         # Боковая панель
         self.side_panel = QWidget(self.main_widget)
-        self.side_panel.setFixedWidth(190) 
+        self.side_panel.setMaximumWidth(190) 
         self.side_panel.setStyleSheet("""
             background-color: #FFFFFF;
             border-radius: 16px;
             """)
         
-        self.main_layout.addWidget(self.side_panel, 2)
         
         # Лайаут боковой панели
         self.side_panel_layout = QVBoxLayout(self.side_panel)
+        self.side_panel_layout.setContentsMargins(10, 10, 10, 10)
+        
+        self.side_panel_layout.setSpacing(10)
+    
 
-        
-        
+        self.side_panel_layout.addStretch(1)
 
-        
-        # Панель контента
-        self.content_panel = QStackedWidget(self.main_widget)
-        self.setFixedWidth(600)
-        self.content_panel.setStyleSheet("""
-            background-color: #FFFFFF;
-            border-radius: 16px;
-            """)
+
+        self.main_layout.addWidget(self.side_panel, 2)
         self.main_layout.addWidget(self.content_panel, 7)
 
 
-        
-        
-        
-        self.pages = [
-            Settings(),
-            Profile(),
-            VoiceInput(),
-            TextInput(),
-            GesturesInput(),
-            Dictator()
-        ]
-        
 
 
 
+
+       
+
+        
+
+        
+
+class ContentPageWidget(QWidget):
+    def __init__(self, master):
+        super().__init__(master)
+        
+        self.side_panel_button = QPushButton()
+        self.side_panel_button.setStyleSheet("""    
+            background-color: #D9D9D9;            
+            border-radius: 12px;
+            color: #000000;
+            font-size: 16px;
+            font-family: "Roboto";                            
+            """)          
+        self.side_panel_button.setMinimumSize(160, 60)
+        
 # ===========================================================
 # НАСТРОЙКИ
 # ===========================================================
-class Settings(QWidget):
-    def __init__(self):
-        super().__init__()
-
+class Settings(ContentPageWidget):
+    def __init__(self, master):
+        super().__init__(master)
+        self.side_panel_button.setText("Настройки") 
+        
 
 # ===========================================================
 # ПРОФИЛЬ
 # ===========================================================
-class Profile(QWidget):
-    def __init__(self):
-        super().__init__()
-
+class Profile(ContentPageWidget):
+    def __init__(self, master):
+        super().__init__(master)
+        self.side_panel_button.setText("Профиль") 
 
 # ===========================================================
 # ГОЛОСОВОЙ ВВОД
 # ===========================================================
-class VoiceInput(QWidget):
-    def __init__(self):
-        super().__init__()
-
+class VoiceInput(ContentPageWidget):
+    def __init__(self, master):
+        super().__init__(master)
+        self.side_panel_button.setText("Голосовой Ввод") 
 
 # ===========================================================
 # ТЕКСТОВЫЙ ВВОД
 # ===========================================================
-class TextInput(QWidget):
-    def __init__(self):
-        super().__init__()
-
+class TextInput(ContentPageWidget):
+    def __init__(self, master):
+        super().__init__(master)
+        self.side_panel_button.setText("Текстовый Ввод") 
 
 # ===========================================================
 # ЖЕСТОВЫЙ ВВОД
 # ===========================================================
-class GesturesInput(QWidget):
-    def __init__(self):
-        super().__init__()
-
+class GesturesInput(ContentPageWidget):
+    def __init__(self, master):
+        super().__init__(master)
+        self.side_panel_button.setText("Жестовый Ввод") 
 
 # ===========================================================
 # ДИКТОР
 # ===========================================================
-class Dictator(QWidget):
+class ScreenReader(QWidget):
     def __init__(self):
         super().__init__()
 
