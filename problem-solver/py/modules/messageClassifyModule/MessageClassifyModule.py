a65bd5135aa7ebe3a83f0739d147a2b93507df2b
Ivan Schetcko <meliodasochek9@gmail.com>
2026-04-25 20:08:50 +0300

    Удалил ненужное

--- problem-solver/py/modules/messageClassifyModule/MessageClassifyModule.py
+++
@@ -1,7 +0,0 @@
-from sc_kpm import ScModule
-from .MessageClassifyAgent import MessageClassifyAgent
-
-
-class MessageClassifyModule(ScModule):
-    def __init__(self):
-        super().__init__(MessageClassifyAgent())
